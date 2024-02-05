b390997521bc1160b5fbeb250b779cf309fba9f0
velox0 <72274796+Velox@users.noreply.github.com>
2024-02-05 17:35:04 +0530

    updated display.c: shortened noraml colour string

--- src/display/display.c
+++ src/display/display.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 void display24(const char *str, colour24 colour) {
-  char normal[] = "\033[0;0m";
+  char normal[] = "\033[0m";
   // "Activate" formatting by printing escape code
   printf("\033[38;%d;%d;%d;%d;48;%d;%d;%d;%dm", colour[FCID], colour[FR],
          colour[FG], colour[FB], colour[BCID], colour[BR], colour[BG],
@@ -14,7 +14,7 @@ void display24(const char *str, colour24 colour) {
 }
 
 void display8(const char *str, colour8 colour) {
-  char normal[] = "\033[0;0m";
+  char normal[] = "\033[0m";
   // "Activate" formatting by printing escape code
   printf("\033[38;%d;%d;48;%d;%dm", colour[FCID8], colour[FN], colour[BCID8],
          colour[BN]);
@@ -26,7 +26,7 @@ void display8(const char *str, colour8 colour) {
 
 void basic_display(const char *str, basic_colour colour,
                    enum colour_index control) {
-  char normal[] = "\033[0;0m";
+  char normal[] = "\033[0m";
   switch (control) {
   case 0:
   case NOBG:
