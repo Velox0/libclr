f2a7d6733b2bee1ee6558cf44991127794819e27
velox0 <veloxzerror@gmail.com>
2024-02-17 17:22:36 +0530

    updadte: colourmods.h - removed obsolete interpolate() declaration

--- include/colourmods.h
+++ include/colourmods.h
@@ -190,9 +190,6 @@ inline void resetfg24(colour24 colour) { colour[FCID] = 0; }
 */
 inline void resetbg24(colour24 colour) { colour[BCID] = 0; }
 
-void interpolate24(colour24 colour, colour24 colour1, colour24 colour2,
-                   float factor);
-
 /*
   Math function
   Takes 3 colour24's:
