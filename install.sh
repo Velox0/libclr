bdec78b9e0883786cc4f59fe9529257b6d095e4a
velox0 <veloxzerror@gmail.com>
2024-02-26 15:53:38 +0530

    add: install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if [ ! -d ./lib ]; then
+    mkdir -p lib
+fi
+
+gcc -Wall -shared -fPIC -o lib/libclr.so src/colourmods.c src/display.c -Iinclude
+
+if [ ! -d /usr/include/libclr ]; then
+    sudo mkdir -p /usr/include/libclr/
+fi
+
+sudo cp ./include/* /usr/include/libclr/
+
+sudo mv ./lib/libclr.so /usr/lib/
